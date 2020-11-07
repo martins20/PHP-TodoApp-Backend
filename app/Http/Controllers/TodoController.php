227d6fd58266fa5c35416f47423fbd856e81dfd6
martins20 <pdesignerpb@gmail.com>
2020-11-06 23:44:31 -0300

    Feature: user can create a todo

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function __construct(Todo $todo) {
+        $this->todo = $todo;
+    }
+
+    public function store (Request $request) {
+
+        // Get todo_name of all request types
+        $todo_name = $request->input('todo_name');
+
+        // Throw a new error in response if todo_name don't exists
+        if(!isset($todo_name)) return ['error'=>'todo_name is required'];
+
+        // Get is_completed of all request types and if it don't exists false is default value
+        $is_completed = $request->input('is_completed', false);
+
+
+        $response = $this->todo->create($request->all());
+        return $response;
+    }
+}
